5f4181745a36cc46498ae786d4cccaea2fd25dbc
sophia-bouck <sophia.bouck@gmail.com>
2024-05-30 13:54:54 -0700

    buggy versions of the users endpoints

--- api/users.js
+++ api/users.js
@@ -1,30 +1,106 @@
-const { Router } = require('express')
-const { ValidationError } = require('sequelize')
+const { Router } = require("express");
+const { ValidationError } = require("sequelize");
+const bcrypt = require("bcryptjs");
 
-//adding models 
-const { Assignment, AssignmentClientFields, getAssignmentById } = require('../models/assignment') //dont think you will need this 
-const { Submission, SubmissionClientField, insertNewSubmission } = require('../models/submission') // or this 
-const { UserSchema, getUserById } = require('../models/user')
-const { Course, getCourseById } = require('../models/course') // or this 
+//adding models
+const {
+  Assignment,
+  AssignmentClientFields,
+  getAssignmentById,
+} = require("../models/assignment"); //dont think you will need this
+const {
+  Submission,
+  SubmissionClientField,
+  insertNewSubmission,
+} = require("../models/submission"); // or this
+const {
+  UserSchema,
+  getUserById,
+  validateCredentials,
+} = require("../models/user");
+const { Course, getCourseById } = require("../models/course"); // or this
 
-//adding auth 
-const { generateAuthToken, requireAuthentication } = require('../lib/auth')
+//adding auth
+const {
+  generateAuthToken,
+  requireAuthentication,
+  checkIfAdmin,
+} = require("../lib/auth");
 
-const router = Router()
+const router = Router();
 
 //POST //users
-// Create and store a new application User with specified data and adds it to the application's database.  
+// Create and store a new application User with specified data and adds it to the application's database.
 // Only an authenticated User with 'admin' role can create users with the 'admin' or 'instructor' roles.
+router.post("/", checkIfAdmin, async function (req, res, next) {
+  try {
+    // deal with password hashing here
+    const hash = await bcrypt.hash(req.body.password, 8);
+    console.log("the hash!::: ", hash);
+    //
+    const user = await UserSchema.create(
+      { ...req.body, password: hash },
+      UserClientFields
+    );
+    res.status(201).send({ id: user.id });
+  } catch (e) {
+    if (e instanceof ValidationError) {
+      res.status(400).send({ error: e.message });
+    } else {
+      next(e);
+    }
+  }
+});
 
 //POST //users/login
 // Authenticate a specific User with their email address and password.
+router.post("/login", async function (req, res, next) {
+  try {
+    const auth = await validateCredentials(req.body.id, req.body.password);
+    if (auth) {
+      const token = generateAuthToken(req.body.id);
+      res.status(200).send({
+        token: token,
+      });
+    } else {
+      res.status(401).send({
+        error: "Invalid Authentication Credentials",
+      });
+    }
+  } catch (e) {
+    next(e);
+  }
+});
 
 //GET //users/:id
-// Returns information about the specified User.  If the User has the 'instructor' role, the response should include a list of the IDs of the 
-//Courses the User teaches (i.e. Courses whose instructorId field matches the ID of this User).  If the User has the 'student' role, the response 
-//should include a list of the IDs of the Courses the User is enrolled in.  Only an authenticated User whose ID matches the ID of the requested User 
+// Returns information about the specified User.  If the User has the 'instructor' role, the response should include a list of the IDs of the
+//Courses the User teaches (i.e. Courses whose instructorId field matches the ID of this User).  If the User has the 'student' role, the response
+//should include a list of the IDs of the Courses the User is enrolled in.  Only an authenticated User whose ID matches the ID of the requested User
 //can fetch this information.
+router.get("/:userId", requireAuthentication, async function (req, res, next) {
+  // get user based on userid, then check admin for authorization check
+  const user = await getUserById(req.user);
+  if (!user.admin) {
+    if (req.user != req.params.userId) {
+      res.status(403).send({
+        error: "Not Authorized to Access",
+      });
+    }
+  }
+  const userId = req.params.userId;
+  try {
+    const user = await UserSchema.findByPk(userId, {
+      // exclude field named password
+      attributes: { exclude: ["password"] },
+    });
+    if (user) {
+      res.status(200).send(user);
+    } else {
+      next();
+    }
+  } catch (e) {
+    next(e);
+  }
+});
 
-
-
-module.exports = router
+module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,51 +1,54 @@
-const { DataTypes } = require('sequelize')
-const sequelize = require('../lib/sequelize')
+const { DataTypes } = require("sequelize");
+const sequelize = require("../lib/sequelize");
 
-const bcrypt = require('bcryptjs')
-const { extractValidFields } = require('../lib/validation')
+const bcrypt = require("bcryptjs");
+const { extractValidFields } = require("../lib/validation");
 
-
-const UserSchema = sequelize.define('user', {
-  userID: { type: DataTypes.INTEGER, allowNull: false},
-  email: { type: DataTypes.STRING, allowNull: false, unique: true},
+const UserSchema = sequelize.define("user", {
+  userID: { type: DataTypes.INTEGER, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
   password: { type: DataTypes.STRING, allowNull: true },
-  role: {type: DataTypes.STRING, allowNull: false},
-  courseId: {type: DataTypes.STRING, allowNull: true}
-})
+  role: { type: DataTypes.STRING, allowNull: false },
+  courseId: { type: DataTypes.STRING, allowNull: true },
+});
 
-exports.UserSchema = UserSchema
-exports.UserClientFields = [
-  'userID',
-  'email',
-  'password',
-  'role',
-  'courseId'
-]
+exports.UserSchema = UserSchema;
+exports.UserClientFields = ["userID", "email", "password", "role", "courseId"];
+
+exports.validateCredentials = async function (id, password) {
+  //get user by id, including password in return
+  const user = await UserSchema.findByPk(id, {});
+  //decypt pass
+  if (user) {
+    return bcrypt.compare(password, user.password);
+  } else {
+    return false;
+  }
+};
 
 exports.insertNewUser = async function (user) {
-  user.password = await bcrypt.hash(user.password, 8)
-  console.log("Hashed password:", user.password)
-  const result = await UserSchema.create(user, exports.UserClientFields)
-  return result.id
-}
+  user.password = await bcrypt.hash(user.password, 8);
+  console.log("Hashed password:", user.password);
+  const result = await UserSchema.create(user, exports.UserClientFields);
+  return result.id;
+};
 
 exports.getUserById = async function (id, includePassword) {
-  const user = await UserSchema.findByPk(id)
-  if(!includePassword && user){
+  const user = await UserSchema.findByPk(id);
+  if (!includePassword && user) {
     user.password = 0;
   }
-   return user
-}
-
+  return user;
+};
 
 exports.getUserByEmail = async function (userEmail, includePassword) {
   const user = await UserSchema.findAll({
-        where:{ 
-           email: userEmail
-        }
-  })
-  if(!includePassword){
+    where: {
+      email: userEmail,
+    },
+  });
+  if (!includePassword) {
     user[0].password = 0;
   }
-  return user[0]
-}
+  return user[0];
+};
