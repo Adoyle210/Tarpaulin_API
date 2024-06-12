b915ddad4336d2b48dffd2390b73732205bde7e0
lacim <lacimonsrud@gmail.com>
2024-06-12 01:08:35 -0700

    added patch to submissions

--- api/submissions.js
+++ api/submissions.js
@@ -23,6 +23,18 @@ const router = Router();
 //PATCH //submissions/:id
 // Performs a partial update on the data for the Submission.  This is the only way to assign a grade to a Submission.  Only an authenticated User with 'admin'
 //role or an authenticated 'instructor' User whose ID matches the instructorId of the associated course can update a Submission.
+router.patch("/:id", requireAuthentication, async function (req, res, next) {
+  const submissionId = req.params.submissionId;
+  const result = await Submission.update(req.body, {
+    where: { id: submissionId },
+    fields: SubmissionClientField,
+  });
+  if (result[0] > 0) {
+    res.status(204).send();
+  } else {
+    next();
+  }
+});
 
 //GET //media/submissions/:filename
 //Download a Submission's associated file.  Only an authenticated User with 'admin' role or an
