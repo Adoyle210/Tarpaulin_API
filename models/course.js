e8f88544ea80d838f1140cd6e72d0a866161dd2f
Lauren Campbell <62973230+LaurenRenaeCampbell@users.noreply.github.com>
2024-06-11 21:48:11 -0700

    Update course.js

--- models/course.js
+++ models/course.js
@@ -1,54 +1,39 @@
-const { DataTypes } = require('sequelize')
-const { UserSchema } = require("./user")
+const { DataTypes } = require("sequelize");
+const { User } = require("./user");
 
-const sequelize = require('../lib/sequelize')
-const { Enrollment } = require("./enrollment");
+const sequelize = require("../lib/sequelize");
 
-const Course = sequelize.define('course', {
-  courseID:{ type: DataTypes.STRING, allowNull: false },
-  subject:{type: DataTypes.STRING, allowNull: false },
+const Course = sequelize.define("course", {
+  // courseID: { type: DataTypes.STRING, allowNull: false },
+  subject: { type: DataTypes.STRING, allowNull: false },
   number: { type: DataTypes.STRING, allowNull: false },
   title: { type: DataTypes.STRING, allowNull: false },
   term: { type: DataTypes.STRING, allowNull: false },
-  instructorId: { type: DataTypes.INTEGER, allowNull: false }
-})
+  instructorId: { type: DataTypes.INTEGER, allowNull: false },
+});
 
-exports.Course = Course
+exports.Course = Course;
 exports.CourseClientField = [
-  'courseID',
-  'subject',
-  'number',
-  'title',
-  'term',
-  'instructorId',
-]
+  // "courseID",
+  "subject",
+  "number",
+  "title",
+  "term",
+  "instructorId",
+];
 
+// const Enrollment = sequelize.define("enrollment", {});
+
+// Course.belongsToMany(User, { through: Enrollment });
+// User.belongsToMany(Course, { through: Enrollment });
 
 exports.insertNewCourse = async function (course) {
-  const result = await Course.create(course, exports.CourseClientField)
-  return result.id
-}
+  const result = await Course.create(course, exports.CourseClientField);
+  return result.id;
+};
 
 exports.getCourseById = async function (id) {
-  const course = await Course.findByPk(id)
-  return course
-}
+  const course = await Course.findByPk(id);
+  return course;
+};
 
-exports.getEnrolledStudents = async function (courseId) {
-  const enrollments = await Enrollment.findAll({ where: { courseId } });
-  return enrollments.map(enrollment => enrollment.userId);
-}
-
-exports.enrollStudents = async function (courseId, studentIds) {
-  const enrollments = studentIds.map(studentId => ({ courseId, studentId }));
-  await Enrollment.bulkCreate(enrollments, { ignoreDuplicates: true });
-}
-
-exports.unenrollStudents = async function (courseId, studentIds) {
-  await Enrollment.destroy({
-    where: {
-      courseId,
-      studentId: studentIds
-    }
-  })
-}
