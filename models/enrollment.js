9543b2b9eda172aa97b05ee5be752efa89a57795
Lauren Campbell <62973230+LaurenRenaeCampbell@users.noreply.github.com>
2024-06-11 21:48:34 -0700

    Update enrollment.js

--- models/enrollment.js
+++ models/enrollment.js
@@ -1,9 +1,27 @@
+// models/enrollment.js
 const { DataTypes } = require('sequelize');
 const sequelize = require('../lib/sequelize');
 
-const Enrollment = sequelize.define('enrollment', {
-  courseId: { type: DataTypes.INTEGER, allowNull: false },
-  userId: { type: DataTypes.INTEGER, allowNull: false }
+const Enrollment = sequelize.define('Enrollment', {
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'User',
+      key: 'id'
+    }
+  },
+  courseId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Course',
+      key: 'id'
+    }
+  }
+}, {
+  tableName: 'enrollment',
+  timestamps: false
 });
 
-exports.Enrollment = Enrollment;
+module.exports = Enrollment;
