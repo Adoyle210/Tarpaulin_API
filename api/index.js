2bf978142b0dabe7fe75f70b1f28df8509d4d303
Lauren Campbell <62973230+LaurenRenaeCampbell@users.noreply.github.com>
2024-06-11 21:45:17 -0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
-const { Router } = require('express')
-const router = Router()
+const { Router } = require("express");
+const { User } = require("../models/user");
+const { Course } = require("../models/course");
+const router = Router();
 
-router.use('/users', require('./users'))
-router.use('/assignments', require('./assignments'))
-router.use('/courses', require('./courses'))
-router.use('/submissions', require('./submissions'))
+router.use("/users", require("./users"));
+router.use("/assignments", require("./assignments"));
+router.use("/courses", require("./courses"));
+router.use("/submissions", require("./submissions"));
 
-module.exports = router
+// Define the many-to-many relationship between User and Course
+User.belongsToMany(Course, { through: "enrollment" });
+Course.belongsToMany(User, { through: "enrollment" });
+
+module.exports = router;
